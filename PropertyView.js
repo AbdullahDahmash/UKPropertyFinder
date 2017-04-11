5d620a61e9332da7db4e14600ca5ac9271a81b09
Abdullah <Abdullah.dahmash@yahoo.com>
2017-04-11 11:19:40 +0300

    added Google map to propery view

--- PropertyView.js
+++ PropertyView.js
@@ -3,12 +3,18 @@ import {
   StyleSheet,
   Image,
   View,
-  Text
+  Text,
+  ScrollView,
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 let styles = StyleSheet.create({
   container: {
     marginTop: 65
+  },
+  map: {
+    height: 200,
+    margin: 40,
   },
   heading: {
     backgroundColor: '#F8F8F8',
@@ -48,18 +54,36 @@ export default class PropertyView extends Component {
       stats += ', ' + property.bathroom_number + ' ' + (property.bathroom_number > 1 ? 'bathrooms' : 'bathroom');
     }
     let price = property.price_formatted.split(' ')[0];
-    
+
     return (
-      <View style={styles.container}>
-        <Image style={styles.image} source={{uri: property.img_url}} />
-        <View style={styles.heading}>
-          <Text style={styles.price}>{price}</Text>
-          <Text style={styles.title}>{property.title}</Text>
-          <View style={styles.separator}/>
-        </View>
-        <Text style={styles.description}>{stats}</Text>
-        <Text style={styles.description}>{property.summary}</Text>
+      <ScrollView style={styles.container}>
+      <Image style={styles.image} source={{uri: property.img_url}} />
+      <View style={styles.heading}>
+      <Text style={styles.price}>{price}</Text>
+      <Text style={styles.title}>{property.title}</Text>
+      <View style={styles.separator}/>
       </View>
+      <Text style={styles.description}>{stats}</Text>
+      <Text style={styles.description}>{property.summary}</Text>
+      <Text style={styles.description}>{stats}</Text>
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: property.latitude,
+          longitude: property.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+      <MapView.Marker
+        coordinate={{
+          longitude: property.longitude,
+          latitude: property.latitude,
+        }}
+        title={property.title}
+        description={property.summary}
+      />
+      </MapView>
+      </ScrollView>
     )
   }
 }
